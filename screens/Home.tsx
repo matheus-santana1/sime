b4c284f9a73706409d60a45e1084d7040cddd56f
Matheus Santana <e16504@ifmg.edu.br>
2024-10-20 22:08:49 -0300

    Prepara a base do app criando pages e routes.

---
+++ screens/Home.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function AppScreen() {
+  return (
+    <>
+      <View className="flex-1 items-center bg-gray-500">
+        <Text>Home Screen</Text>
+      </View>
+    </>
+  );
+}
